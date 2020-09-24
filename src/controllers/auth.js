72586823a4241cda50053ba0a1c33fdb72a8fbd2
Lucas Marins <lmarins02@hotmail.com.br>
2020-09-24 19:38:59 -0300

    Storing users accounts into detabase

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,13 +1,25 @@
 const express = require('express');
-
+const {Account} = require('../models')
+const bcrypt = require('bcrypt');
 const router = express.Router();
+
+const saltRounds = 10;
 
 router.get('/sign-in',(req,res) =>{
     return res.json('Sign in');
 })
 
-router.get('/sign-up',(req,res)=>{
-    return res.json('Sign out')
+router.get('/sign-up',async (req,res)=>{
+
+    const email = 'lmarins02@hotmail.com.br'
+    const password ='123456'
+
+    const hash  = bcrypt.hashSync(password, saltRounds)
+
+    const result = await Account.create({email,password:hash})
+    console.log(hash)
+    
+  return res.json(result)
 })
 
 module.exports = router
