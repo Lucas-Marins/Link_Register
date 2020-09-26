52dafa7e6655e5f3c5f95843ee88e0e94902373e
Lucas Marins <lmarins02@hotmail.com.br>
2020-09-25 22:15:45 -0300

    Links CRUD

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const response = require('./middlewares/response')
 
 const authController = require('./controllers/auth')
 
+const linkController = require('./controllers/link')
+
 const app = express();
 
 app.use(response)
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 app.use('/auth', authController)
+app.use('/link', linkController)
 
 app.get('/',(req,res)=> {
     return res.json('Api running...');
