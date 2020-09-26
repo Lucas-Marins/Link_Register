dbeefc6fd17898fc6d43b3fd67d4cf6572bfd05c
Lucas Marins <lmarins02@hotmail.com.br>
2020-09-25 22:49:09 -0300

    Excluded auth paths from JWT

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,6 +1,13 @@
 const {verifyJwt} = require('../helpers/jwt')
 
 const checkJwt = (req, res ,next) =>{
+    
+    const {url: path } = req;
+
+    const excludedPaths = ['/auth/sign-in', '/auth/sign-up'];
+    const isExcluded = excludedPaths.find(p=> p.startsWith(path))
+    if(isExcluded) return next();
+
     let token = req.headers['authorization'];
     token  = token ? token.slice(7 , token.lenght): null;
     if(!token){
@@ -10,6 +17,7 @@ const checkJwt = (req, res ,next) =>{
     try{
         const decoded = verifyJwt(token)
         req.accontId = decoded.id;
+        next();
     }catch{
         return res.jsonUnauthorized(null, 'Invalid token');
     }
